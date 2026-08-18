44194307eae396d0febd04b0fc5761a9cf2de6e8
Retro Developer <developer@retro.com>
2026-08-18 20:38:27 +0300

    fix: allow all external image domains for google image links

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
   },
   experimental: {
     cpus: 1,
